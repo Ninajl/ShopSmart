952d1bfd74369b5515fd574f09de4500e3cd1793
Nick Peters <nick@barbetsapp.com>
2015-05-12 12:00:37 -0600

    set up amazon model/view/api

--- app/controllers/amazon_fetcher_controller.rb
+++ app/controllers/amazon_fetcher_controller.rb
@@ -1,9 +1,9 @@
-class WelcomeController < ApplicationController
+class AmazonFetcherController < ApplicationController
 
   def index
     @amazon = AmazonFetcher.new
     @response = @amazon.amazon_search
     @products = @response['ItemSearchResponse']['Items']['Item']
   end
-  
+
 end
